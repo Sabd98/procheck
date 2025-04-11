2d9d80d33fe7bfaf50e9d3a0cf2f08d874577978
Sabda <sabdaavicennaa@gmail.com>
2025-04-11 19:58:22 +0700

    add setHeader

--- BE-PROCHECK/index.js
+++ BE-PROCHECK/index.js
@@ -23,6 +23,15 @@ app.use(cors({ origin: 'https://fe-procheck.vercel.app/' }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use('/projects', projectRoutes);
 app.use('/tasks', taskRoutes);
