f4c6fab2b9341827dea774f1e8ff75e80ca58f23
Sabda <sabdaavicennaa@gmail.com>
2025-04-22 13:51:28 +0700

    Change BE domain

--- FE-PROCHECK/src/api/api.js
+++ FE-PROCHECK/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://be-procheck-57f2af579b8e.herokuapp.com',
-    headers: {
-        'Content-Type': 'application/json'
-      }
-})
+  baseURL: "https://procheck-production.up.railway.app/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
